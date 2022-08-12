3729f2679a67def50e6db31b6665ed7142131a26
ssj24 <sujee0824@gmail.com>
2022-08-12 22:33:29 +0900

    remove route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Route, Routes} from 'react-router-dom';
 import MovieList from './components/MovieList.js';
 import ErrorBoundary from './components/ErrorBoundary.js';
 import './App.css';
@@ -11,11 +10,7 @@ function App() {
         Movie List
       </header>
       <ErrorBoundary>
-        <Routes>
-          <Route path="/movie" element={
-              <MovieList />
-          }/>
-        </Routes>
+        <MovieList />
       </ErrorBoundary>
     </div>
   );
